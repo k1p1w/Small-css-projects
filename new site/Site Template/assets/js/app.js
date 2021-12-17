d9fb3738eff572b7e34a6c99654b83c670c2a2df
k1p1w <k1p1wzbs@gmail.com>
2021-12-17 23:16:01 +0200

    test-site

---
+++ new site/Site Template/assets/js/app.js
@@ -0,0 +1,49 @@
+$(function () {
+
+	var header = $("#header"),
+		inroH = $("#intro").innerHeight(),
+		scrollOffset = $(window).scrollTop();
+
+	/* Fixed header */
+	checkScroll(scrollOffset);
+
+	$(window).on("scroll", function () {
+		scrollOffset = $(this).scrollTop();
+
+		checkScroll(scrollOffset);
+	});
+
+	function checkScroll(scrollOffset) {
+		if (scrollOffset >= inroH) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed")
+		}
+	}
+
+	/* Smooth scroll */
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data('scroll'),
+			blockOffset = $(blockId).offset().top;
+
+		$("#nav a").removeClass("active");
+		$this.addClass("active");
+
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 500);
+	})
+
+	/* Menu nav toggle */
+	$("#nav_toggle").on("click", function (event) {
+		event.preventDefault();
+
+		$(this).toggleClass("active");
+		$("#nav").toggleClass("active");
+	});
+
+
+});
